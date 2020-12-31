dff1537f950560bab6ac8319f6ae3151521011db
Danny Ben Shitrit <db@dannyben.com>
2020-12-31 19:05:15 +0000

    fixes

--- src/support.rb
+++ src/support.rb
@@ -27,23 +27,6 @@ def featured
   @featured ||= YAML.load_file 'src/config/featured.yml'
 end
 
-# def toc
-#   @toc ||= toc!
-# end
-
-# def toc!
-#   result = repos_by_tag.map do |tag, repos|
-#     link = tag_to_link(tag)
-#     { key: tag, link: link,  count: repos.count }
-#   end
-
-#   result.sort_by { |a| a[:link] }
-# end
-
-# def user_content_prefix
-#   ENV['DEV'] ? 'user-content-' : ''
-# end
-
 def repos_by_tag
   @repos_by_tag ||= repos_by_tag!
 end
@@ -62,20 +45,3 @@ def repos_by_tag!
 
   result.sort_by { |k, v| [-v.count, k] }.to_h
 end
-
-# def tag_to_link(tag)
-#   "[#{tag_title(tag)}](##{user_content_prefix}#{slug(tag)})"
-# end
-
-# def slug(tag)
-#   tag_title(tag).downcase.gsub(' ', '-').gsub(/[^\w\-]/, '')
-# end
-
-# def tag_title(tag)
-#   if tags[tag]
-#     tags[tag]
-#   else
-#     $stderr.puts "Warning: no title for #{tag}"
-#     tag
-#   end
-# end
